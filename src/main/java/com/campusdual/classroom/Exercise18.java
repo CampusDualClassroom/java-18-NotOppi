7290ce8529e60676897e2a3e6e6138394a663c5a
NotOppi <Filippofg@outlook.com>
2025-05-26 00:01:02 +0200

    Listo

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,38 @@
 package com.campusdual.classroom;
+import com.campusdual.util.Utils;
 
 public class Exercise18 {
 
 	public static void main(String[] args) {
 
+		int index = Utils.integer("Introduzca la cantidad de posiciones que desea en el array: ");
+		int[] arreglo = createAndInitializeArray(index);
+		showInlineArray(arreglo);
+
 	}
+
+	public static int[] createAndInitializeArray(int index){
+
+		int[] arreglo = new int[index];
+		for (int i = 0; i < index; i++) {
+			arreglo[i] = i+1;
+		}
+
+		return arreglo;
+	}
+
+	public static void showInlineArray(int[] arreglo){
+
+		for (int j = 0; j < arreglo.length; j++) {
+			System.out.print(arreglo[j]);
+			if (j < arreglo.length - 1) {
+				System.out.print(" ");
+			}
+		}
+
+
+	}
+
+
+
 }
